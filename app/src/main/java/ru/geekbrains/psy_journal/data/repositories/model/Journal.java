4fcd592ce9cd7143809a997f6efabce8a63107ec
Artem <kunakbaevav@yandex.ru>
2019-07-14 11:03:26 +0500

    Убрал индексацию

--- app/src/main/java/ru/geekbrains/psy_journal/data/repositories/model/Journal.java
+++ app/src/main/java/ru/geekbrains/psy_journal/data/repositories/model/Journal.java
@@ -3,7 +3,6 @@ package ru.geekbrains.psy_journal.data.repositories.model;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
@@ -25,13 +24,10 @@ public class Journal implements Parcelable {
     private Float workTime = 1.0f;
     private int idCategory;
     private int idGroup;
-    //    @ColumnInfo(index = true)
     private String name;
-    //    @ColumnInfo(index = true)
     private String declaredRequest;
     private String realRequest;
     private int idWorkForm;
-    //    @ColumnInfo(index = true)
     private String codeTd;
     private String comment;
 
