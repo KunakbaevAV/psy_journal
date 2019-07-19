71f1a6bcab34be18e2eec7ec042c40124006405c
Artem <kunakbaevav@yandex.ru>
2019-07-19 13:53:37 +0500

    Скорректировал ошибку с указанием пути к файлу с профстандартом

--- app/src/main/java/ru/geekbrains/psy_journal/presentation/view/activities/MainActivity.java
+++ app/src/main/java/ru/geekbrains/psy_journal/presentation/view/activities/MainActivity.java
@@ -51,28 +51,33 @@ import static ru.geekbrains.psy_journal.Constants.TAG_ALL_WORK;
 
 public class MainActivity extends MvpAppCompatActivity implements InformedView {
 
-	@BindView(R.id.main_navigation_drawer) DrawerLayout drawer;
-	@BindView(R.id.navigation_view) NavigationView navigationView;
-	@BindView(R.id.bottomAppBar) BottomAppBar bottomAppBar;
-	@BindView(R.id.fab) FloatingActionButton fab;
-    @BindDrawable(R.drawable.ic_add_circle_outline_white_24dp) Drawable plus;
-    @BindDrawable(R.drawable.ic_done_white_24dp) Drawable done;
+    private static final int REQUEST_PERMISSION_CREATE_FILE_XLS = 1;
+    private static final int REQUEST_PERMISSION_READ_FILE_XML = 2;
+    private static final int REQUEST_PERMISSION_READ_FILE_XLS = 3;
+    private static final int REQUEST_FILES_GET = 4;
+    private static final int REQUEST_XML_FILE_GET = 5;
+    @BindView(R.id.main_navigation_drawer)
+    DrawerLayout drawer;
+    @BindView(R.id.navigation_view)
+    NavigationView navigationView;
+    @BindView(R.id.bottomAppBar)
+    BottomAppBar bottomAppBar;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindDrawable(R.drawable.ic_add_circle_outline_white_24dp)
+    Drawable plus;
+    @BindDrawable(R.drawable.ic_done_white_24dp)
+    Drawable done;
+    @InjectPresenter
+    MainPresenter mainPresenter;
 
-    @InjectPresenter MainPresenter mainPresenter;
-
-	private static final int REQUEST_PERMISSION_CREATE_FILE_XLS = 1;
-	private static final int REQUEST_PERMISSION_READ_FILE_XML = 2;
-	private static final int REQUEST_PERMISSION_READ_FILE_XLS = 3;
-	private static final int REQUEST_FILES_GET = 4;
-	private static final int REQUEST_XML_FILE_GET = 5;
-
-	@ProvidePresenter
-	MainPresenter providePresenter(){
-		MainPresenter mainPresenter = new MainPresenter();
-		App.getAppComponent().inject(mainPresenter);
-		mainPresenter.checkDataBase();
-		return mainPresenter;
-	}
+    @ProvidePresenter
+    MainPresenter providePresenter() {
+        MainPresenter mainPresenter = new MainPresenter();
+        App.getAppComponent().inject(mainPresenter);
+        mainPresenter.checkDataBase();
+        return mainPresenter;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,18 +93,18 @@ public class MainActivity extends MvpAppCompatActivity implements InformedView {
     }
 
     private void init() {
-		setToggle();
+        setToggle();
         navigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
-	    fab.setOnClickListener(this::onClick);
+        fab.setOnClickListener(this::onClick);
     }
 
-    private void setToggle(){
-	    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-		    this, drawer, bottomAppBar, R.string.int_navigation_drawer_open, R.string.int_navigation_drawer_close);
-	    drawer.addDrawerListener(toggle);
-	    toggle.syncState();
-	    toggle.setDrawerSlideAnimationEnabled(true);
-	    toggle.getDrawerArrowDrawable().setColor(Color.WHITE);
+    private void setToggle() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, bottomAppBar, R.string.int_navigation_drawer_open, R.string.int_navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        toggle.setDrawerSlideAnimationEnabled(true);
+        toggle.getDrawerArrowDrawable().setColor(Color.WHITE);
     }
 
     public void setImageFabForTag(String tag) {
@@ -138,24 +143,24 @@ public class MainActivity extends MvpAppCompatActivity implements InformedView {
 
     private void openAddWorkFragment() {
         getSupportFragmentManager()
-            .beginTransaction()
-            .add(R.id.frame_master, new AddWorkFragment(), TAG_ADD_WORK)
-            .addToBackStack(TAG_ADD_WORK)
-            .commit();
+                .beginTransaction()
+                .add(R.id.frame_master, new AddWorkFragment(), TAG_ADD_WORK)
+                .addToBackStack(TAG_ADD_WORK)
+                .commit();
         setImageFab(done);
     }
 
     private void openAllWorkFragment() {
-	    getSupportFragmentManager().popBackStack(TAG_ADD_WORK, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        getSupportFragmentManager().popBackStack(TAG_ADD_WORK, FragmentManager.POP_BACK_STACK_INCLUSIVE);
         loadFragment(new AllWorkFragment(), TAG_ALL_WORK);
         setImageFab(plus);
     }
 
-    private void loadFragment(Fragment fragment, String tag){
-	    getSupportFragmentManager()
-		    .beginTransaction()
-		    .replace(R.id.frame_master, fragment, tag)
-		    .commit();
+    private void loadFragment(Fragment fragment, String tag) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_master, fragment, tag)
+                .commit();
     }
 
     @Override
@@ -165,76 +170,76 @@ public class MainActivity extends MvpAppCompatActivity implements InformedView {
             drawer.closeDrawer(GravityCompat.START);
             return;
         }
-        if (TAG_ADD_WORK.equals(getTag())){
+        if (TAG_ADD_WORK.equals(getTag())) {
             openAllWorkFragment();
             return;
         }
-	    super.onBackPressed();
+        super.onBackPressed();
     }
 
     private boolean onNavigationItemSelected(@NonNull MenuItem item) {
-	    drawer.closeDrawer(GravityCompat.START);
+        drawer.closeDrawer(GravityCompat.START);
         switch (item.getItemId()) {
-	        case R.id.load_database:
-				loadDataBaseFromXMLFile();
-	        	return true;
-	        case R.id.edit_catalogs:
-		        openScreenEditCatalogs();
-		        return true;
-	        case R.id.get_report:
-		        openScreenGettingReport();
-		        return true;
-	        case R.id.create_excel_report_file:
-	        	createExcelReportFile();
-	        	return true;
-	        case R.id.send_email:
-		        sendToMailReport();
-		        return true;
+            case R.id.load_database:
+                loadDataBaseFromXMLFile();
+                return true;
+            case R.id.edit_catalogs:
+                openScreenEditCatalogs();
+                return true;
+            case R.id.get_report:
+                openScreenGettingReport();
+                return true;
+            case R.id.create_excel_report_file:
+                createExcelReportFile();
+                return true;
+            case R.id.send_email:
+                sendToMailReport();
+                return true;
         }
         return false;
     }
 
-	private void loadDataBaseFromXMLFile(){
-		if (checkPermissions(REQUEST_PERMISSION_READ_FILE_XML)) getXMLFile();
-	}
+    private void loadDataBaseFromXMLFile() {
+        if (checkPermissions(REQUEST_PERMISSION_READ_FILE_XML)) getXMLFile();
+    }
 
     private void openScreenEditCatalogs() {
-	    startActivity(new Intent(getBaseContext(), EditorActivity.class));
+        startActivity(new Intent(getBaseContext(), EditorActivity.class));
     }
 
     private void openScreenGettingReport() {
         new ReportSelectionDialog().show(getSupportFragmentManager(), Constants.TAG_OTF_SELECTION);
     }
 
-    private void createExcelReportFile(){
-		if (checkPermissions(REQUEST_PERMISSION_CREATE_FILE_XLS)) mainPresenter.createExcelFile();
+    private void createExcelReportFile() {
+        if (checkPermissions(REQUEST_PERMISSION_CREATE_FILE_XLS)) mainPresenter.createExcelFile();
     }
 
-    private void sendToMailReport(){
-		if (checkPermissions(REQUEST_PERMISSION_READ_FILE_XLS)) getFiles();
+    private void sendToMailReport() {
+        if (checkPermissions(REQUEST_PERMISSION_READ_FILE_XLS)) getFiles();
     }
 
-	private void getXMLFile(){
-		Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-		intent.setType("application/xml");
-		intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-		startActivityForResult(intent, REQUEST_XML_FILE_GET);
-	}
+    private void getXMLFile() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("application/xml");
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        startActivityForResult(intent, REQUEST_XML_FILE_GET);
+    }
 
-    private void getFiles(){
-	    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+    private void getFiles() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType(INTENT_TYPE_EXCEL);
-	    intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-	    intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-	    startActivityForResult(intent, REQUEST_FILES_GET);
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        startActivityForResult(intent, REQUEST_FILES_GET);
     }
 
     private ArrayList<Uri> getAttachment(ClipData clipData) {
-		ArrayList<Uri> arrayList = new ArrayList<>(clipData.getItemCount());
-	    for (int i = 0; i < clipData.getItemCount(); i++) {
-		    arrayList.add(clipData.getItemAt(i).getUri());
-		}
-	    return arrayList;
+        ArrayList<Uri> arrayList = new ArrayList<>(clipData.getItemCount());
+        for (int i = 0; i < clipData.getItemCount(); i++) {
+            arrayList.add(clipData.getItemAt(i).getUri());
+        }
+        return arrayList;
     }
 
     private Intent sendMultipleFiles(Intent data) {
@@ -253,110 +258,110 @@ public class MainActivity extends MvpAppCompatActivity implements InformedView {
         return intent;
     }
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		switch (requestCode){
-			case REQUEST_FILES_GET:
-				if (resultCode == RESULT_OK && data != null) {
-					sendToMail(data);
-				}
-				break;
-			case REQUEST_XML_FILE_GET:
-				if (resultCode == RESULT_OK && data != null) {
-					loadBase(data);
-				}
-				break;
-		}
-	}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_FILES_GET:
+                if (resultCode == RESULT_OK && data != null) {
+                    sendToMail(data);
+                }
+                break;
+            case REQUEST_XML_FILE_GET:
+                if (resultCode == RESULT_OK && data != null) {
+                    loadBase(data);
+                }
+                break;
+        }
+    }
 
-	private void loadBase(Intent data){
-		Uri uri = data.getData();
-		if (uri != null){
-			String pathFile = getStringPathFile(uri);
-			if (pathFile != null) {
-				try {
-					mainPresenter.loadDataBase(pathFile);
-				} catch (XmlPullParserException e) {
-					showMessage(String.format("Невозможно прочесть файл, %s", e.getDetail()));
-				}
-			}
-		}
-	}
+    private void loadBase(Intent data) {
+        Uri uri = data.getData();
+        if (uri != null) {
+            String pathFile = getStringPathFile(uri);
+            if (pathFile != null) {
+                try {
+                    mainPresenter.loadDataBase(pathFile);
+                } catch (XmlPullParserException e) {
+                    showMessage(String.format("Невозможно прочесть файл, %s", e.getDetail()));
+                }
+            }
+        }
+    }
 
-	private String getStringPathFile(Uri uri){
-		if ("content".equals(uri.getScheme())){
-			String[] projection = new String[]{MediaStore.Files.FileColumns.TITLE};
-			try (Cursor cursor = getContentResolver().query( uri, projection,null, null, null)) {
-				if (cursor != null && cursor.moveToFirst()) {
-					int numberIndex = cursor.getColumnIndex(MediaStore.Files.FileColumns.TITLE);
-					 return cursor.getString(numberIndex);
-				}
-			}
-		}
-		return uri.toString();
-	}
+    private String getStringPathFile(Uri uri) {
+        if ("content".equals(uri.getScheme())) {
+            String[] projection = new String[]{MediaStore.Files.FileColumns.DATA};
+            try (Cursor cursor = getContentResolver().query(uri, projection, null, null, null)) {
+                if (cursor != null && cursor.moveToFirst()) {
+                    int numberIndex = cursor.getColumnIndex(MediaStore.Files.FileColumns.DATA);
+                    return cursor.getString(numberIndex);
+                }
+            }
+        }
+        return uri.toString();
+    }
 
-	private void sendToMail(Intent data){
-		Intent intent = sendMultipleFiles(data);
-		if (intent == null) {
-			intent = sendOneFile(data);
-		}
-		if (intent != null) {
-			intent.setType(INTENT_TYPE_MULTIPART);
-			if (intent.resolveActivity(getPackageManager()) != null) {
-				startActivity(intent);
-			} else showMessage(getString(R.string.need_mail_client));
-		}
-	}
+    private void sendToMail(Intent data) {
+        Intent intent = sendMultipleFiles(data);
+        if (intent == null) {
+            intent = sendOneFile(data);
+        }
+        if (intent != null) {
+            intent.setType(INTENT_TYPE_MULTIPART);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            } else showMessage(getString(R.string.need_mail_client));
+        }
+    }
 
-	private boolean checkPermissions(int request){
-		if (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
-			ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-			ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, request);
-			return false;
-		}
-		return true;
-	}
+    private boolean checkPermissions(int request) {
+        if (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, request);
+            return false;
+        }
+        return true;
+    }
 
-	@Override
-	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
-		if (permissions.length == 2 && (grantResults[0] == PackageManager.PERMISSION_GRANTED &&
-			grantResults[1] == PackageManager.PERMISSION_GRANTED)){
-			switch (requestCode){
-				case REQUEST_PERMISSION_CREATE_FILE_XLS:
-					mainPresenter.createExcelFile();
-					break;
-				case REQUEST_PERMISSION_READ_FILE_XML:
-					getXMLFile();
-					break;
-				case REQUEST_PERMISSION_READ_FILE_XLS:
-					getFiles();
-					break;
-			}
-		} else {
-			showMessage(getString(R.string.no_permission_file));
-		}
-	}
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (permissions.length == 2 && (grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                grantResults[1] == PackageManager.PERMISSION_GRANTED)) {
+            switch (requestCode) {
+                case REQUEST_PERMISSION_CREATE_FILE_XLS:
+                    mainPresenter.createExcelFile();
+                    break;
+                case REQUEST_PERMISSION_READ_FILE_XML:
+                    getXMLFile();
+                    break;
+                case REQUEST_PERMISSION_READ_FILE_XLS:
+                    getFiles();
+                    break;
+            }
+        } else {
+            showMessage(getString(R.string.no_permission_file));
+        }
+    }
 
-	private void showMessage(String message) {
+    private void showMessage(String message) {
         Snackbar snackbar = Snackbar.make(bottomAppBar, message, Snackbar.LENGTH_INDEFINITE);
         snackbar.setAnchorView(fab);
         snackbar.show();
-	}
+    }
 
-	@Override
-	public void showEmpty() {
+    @Override
+    public void showEmpty() {
         showMessage(getString(R.string.db_empty));
-	}
+    }
 
-	@Override
-	public void showGood(String message) {
+    @Override
+    public void showGood(String message) {
         showMessage(String.format(getString(R.string.file_write_to), message));
-	}
+    }
 
-	@Override
-	public void showBad(String error) {
+    @Override
+    public void showBad(String error) {
         showMessage(String.format(getString(R.string.file_write_error), error));
-	}
+    }
 }
